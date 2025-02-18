32621d487197cb541739d1d7ae2ec2a508711a5c
mimoguz <mimoguz@users.noreply.github.com>
2025-02-18 16:25:05 +0300

    Drafting statistics

--- lib/pages/statistics_page/statistics_page.dart
+++ lib/pages/statistics_page/statistics_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:provider/provider.dart';
+import 'package:tripeaks_neue/pages/shared/item_container.dart';
 import 'package:tripeaks_neue/stores/session.dart';
 
 class StatisticsPage extends StatelessWidget {
@@ -14,11 +15,87 @@ class StatisticsPage extends StatelessWidget {
         body: Builder(
           builder: (context) {
             final session = Provider.of<Session>(context);
-            return Center(
+            return Container(
+              color: Theme.of(context).colorScheme.surfaceContainerLow,
               child: Observer(
                 builder: (context) {
                   final statistics = session.statistics;
-                  return Text("Total played: ${statistics.totalGames}\nTotal cleared: ${statistics.cleared}");
+                  return Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      Flexible(
+                        child: SingleChildScrollView(
+                          child: Column(
+                            children: [
+                              ListItemContainer(
+                                child: Card(
+                                  color: Theme.of(context).colorScheme.surface,
+                                  child: Padding(
+                                    padding: const EdgeInsets.all(8.0),
+                                    child: Column(
+                                      children: [
+                                        ListTile(
+                                          title: Text("Total played"),
+                                          trailing: Text(statistics.totalGames.toString()),
+                                        ),
+                                        const Divider(),
+                                        ListTile(
+                                          title: Text("Total cleared"),
+                                          trailing: Text(statistics.cleared.toString()),
+                                        ),
+                                        const Divider(),
+                                        ListTile(
+                                          title: Text("Best score"),
+                                          trailing: Text(
+                                            (statistics.bestGames.firstOrNull?.score ?? 0).toString(),
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              ListItemContainer(child: Text("Last Game")),
+                              ListItemContainer(
+                                child: Card(
+                                  color: Theme.of(context).colorScheme.surface,
+                                  child: Padding(
+                                    padding: const EdgeInsets.all(8.0),
+                                    child: ListTile(
+                                      title: Text(statistics.lastGame?.ended.toIso8601String() ?? "-"),
+                                      subtitle:
+                                          statistics.lastGame?.isCleared ?? false ? Text("Cleared") : null,
+                                      trailing: Text(statistics.lastGame?.score.toString() ?? ""),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              ListItemContainer(child: Text("Best games")),
+                              ListItemContainer(
+                                child: Card(
+                                  color: Theme.of(context).colorScheme.surface,
+                                  child: Padding(
+                                    padding: const EdgeInsets.all(8.0),
+                                    child: Column(
+                                      children: [
+                                        for (final (index, game) in statistics.bestGames.indexed)
+                                          ListTile(
+                                            leading: Text((index + 1).toString()),
+                                            title: Text(game.ended.toIso8601String()),
+                                            subtitle: game.isCleared ? Text("Cleared") : null,
+                                            trailing: Text(game.score.toString()),
+                                          ),
+                                      ],
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  );
                 },
               ),
             );
