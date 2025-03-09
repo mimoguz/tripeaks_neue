37e4a284e75df98a3e87cf8a4af667f46a5177df
mimoguz <mimoguz@users.noreply.github.com>
2025-03-09 09:30:25 +0300

    Remove todo

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -34,8 +34,6 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID
-        // (https://developer.android.com/studio/build/application-id.html).
         applicationId = "io.github.mimoguz.tripeaksneue"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
