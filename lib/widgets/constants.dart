a77dadb26b72c16edee356054399a00edd6331f8
mimoguz <mimoguz@users.noreply.github.com>
2025-04-22 20:51:14 +0300

    Sort constants

--- lib/widgets/constants.dart
+++ lib/widgets/constants.dart
@@ -1,28 +1,30 @@
 import 'package:flutter/widgets.dart';
 
-const double cardSize = 116.0;
-const double commonRadius = 12.0;
-const commonBorderRadius = BorderRadius.all(Radius.circular(commonRadius));
-const double cellPadding = 4.0;
-const double pagePadding = 24.0;
-const double stockShift = 30.0;
-const double buttonSize = 96.0;
-const double maxRealButtonSize = 52.0;
-const double iconScaleThreshold = 0.4;
 const double activeRankSize = 32.0;
 const double activeSuitSize = 32.0;
-const double inactiveRankSize = 24.0;
-const double inactiveSuitSize = 20.0;
-const double requiredWidth = 1260;
-const double requiredHeight = 600;
-const double dropdownItemWidth = 120;
-const double maxListWidth = 700;
-const double maxRadioWidth = maxListWidth - 32;
-const double utilPageMargin = 6.0;
+const double buttonSize = 96.0;
 const double cardPaddingHorizontal = 12.0;
 const double cardPaddingVertical = 10.0;
+const double cardSize = 116.0;
+const double cellPadding = 4.0;
+const double commonRadius = 12.0;
 const double divPadding = 8;
+const double dropdownItemWidth = 120;
+const double iconScaleThreshold = 0.4;
+const double inactiveRankSize = 24.0;
+const double inactiveSuitSize = 20.0;
+const double maxListWidth = 700;
+const double maxRadioWidth = maxListWidth - 32;
+const double maxRealButtonSize = 52.0;
+const double pagePadding = 24.0;
+const double requiredHeight = 600;
+const double requiredWidth = 1260;
+const double stockShift = 30.0;
+const double utilPageMargin = 6.0;
 const double verticalTabsThreshold = 500;
+
+const commonBorderRadius = BorderRadius.all(Radius.circular(commonRadius));
+
 const EdgeInsets cardPadding = EdgeInsets.symmetric(
   horizontal: cardPaddingHorizontal,
   vertical: cardPaddingVertical,
