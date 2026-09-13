feefdd7663a79daf1f4127ee38a6c5cd761c6e6c
Oguz Tas <oguz.tas.devel@gmail.com>
2026-09-13 22:49:28 +0300

    Remove unused

--- lib/pages/home_page/widgets/cards.dart
+++ lib/pages/home_page/widgets/cards.dart
@@ -3,7 +3,6 @@ import 'dart:math';
 import 'package:material_ui/material_ui.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:tripeaks_neue/actions/intents.dart';
-import 'package:tripeaks_neue/assets/custom_icons.dart';
 import 'package:tripeaks_neue/stores/data/back_options.dart';
 import 'package:tripeaks_neue/stores/data/card_value.dart';
 import 'package:tripeaks_neue/stores/tile.dart';
