7370ee558a15ce3458a58e3211eba46775d09ae6
Oguz Tas <oguz.tas.devel@gmail.com>
2026-09-20 16:03:03 +0300

    Suit icon tweaks

--- lib/pages/home_page/widgets/cards.dart
+++ lib/pages/home_page/widgets/cards.dart
@@ -98,17 +98,11 @@ final class const ActiveCard(final Tile tile, {super.key}) extends StatelessWidg
 final class const ActiveCardFace(final CardValue card, {super.key}) extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(bottom: 4.0),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        mainAxisSize: .min,
-        children: [
-          Spacer(),
-          Padding(padding: const EdgeInsets.only(top: 4.0), child: RankText(card)),
-          SuitImage(card),
-          Spacer(),
-        ],
+    return SizedBox(
+      height: c.cardSize,
+      child: Padding(
+        padding: const EdgeInsets.only(top: 1.0),
+        child: Column(mainAxisAlignment: .center, spacing: 5.0, children: [RankText(card), SuitImage(card)]),
       ),
     );
   }
@@ -196,6 +190,7 @@ final class const RankText(final CardValue cardValue, {super.key}) extends State
         fontWeight: .w500,
         color: colour,
         letterSpacing: 1.0,
+        height: 1.0,
       ),
     );
   }
@@ -219,7 +214,13 @@ final class const SuitImage(final CardValue cardValue, {super.key}) extends Stat
     final colour = cardValue.suit.isRed ? colours.tertiary : colours.onSurfaceVariant;
     return Text(
       _suitChar(cardValue.suit),
-      style: TextStyle(fontFamily: "Peckish", fontWeight: .w500, fontSize: 32, color: colour),
+      style: TextStyle(
+        fontFamily: "Peckish",
+        fontWeight: .w500,
+        fontSize: c.activeSuitSize,
+        color: colour,
+        height: 1.0,
+      ),
     );
   }
 }
@@ -254,13 +255,6 @@ final class const TileShadow({super.key, final Alignment centre = Alignment.topR
     );
   }
 }
-
-double _suitCorrection(CardValue card) => switch (card.suit) {
-  Suit.clubs => 2.0,
-  Suit.diamonds => 5.0,
-  Suit.hearts => 2.0,
-  Suit.spades => 2.0,
-};
 
 String _suitChar(Suit suit) => switch (suit) {
   Suit.clubs => "\u2663",
